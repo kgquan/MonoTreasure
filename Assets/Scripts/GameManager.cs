be9950b181206ed8f4567d894bf0b056463f3ccc
Kevin Quan <kevin.quan@hotmail.ca>
2019-08-02 16:34:48 -0600

    Add basic game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MonoTreasure
+{
+    /// <summary>
+    /// Manages the game state.
+    /// </summary>
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager instance = null;
+
+        private void Awake()
+        {
+            if(instance == null)
+            {
+                instance = this;
+            } else if(instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+}
