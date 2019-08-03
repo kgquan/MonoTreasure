a3cf442870346fdccaacaa512f17085a201364ee
Kevin Quan <kevin.quan@hotmail.ca>
2019-08-02 22:39:04 -0600

    Add direction enum; add initial player controller
    Use WASD/arrow keys to move around.

---
+++ Assets/Scripts/Enums/Direction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MonoTreasure.Enums
+{
+    public enum Direction
+    {
+        East,
+        West
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonoTreasure.Enums;
+
+namespace MonoTreasure
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private int moveSpeed = 5;
+        private Direction direction;
+
+        public int MoveSpeed
+        {
+            get; set;
+        }
+
+        private void Awake()
+        {
+            direction = Direction.East;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+            transform.position = transform.position + input * Time.deltaTime * moveSpeed;
+        }
+    }
+}
